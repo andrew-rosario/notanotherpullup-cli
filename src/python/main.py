435f1b00e0d5b93f0c09effbb5547313a47f2d42
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2025-01-24 20:49:37 +0000

    Add initializing database method to Python side

--- src/python/main.py
+++ src/python/main.py
@@ -6,16 +6,27 @@ api_endpoint = "https://api.hevyapp.com/v1/"
 
 
 def initialize_database():
-    conn = sqlite3.connect("database.sqlite3")
-
+    """
+    Initializes the database with the schema.sql file
+    """
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    
+    with open('./schema.sql','r') as file:
+        schema = file.read()
+    
+    cursor.executescript(schema)
+    
+    conn.commit()
+    
+    
 def get_all_initial_workouts():
     i = 1
     done = False
     while not done:
+        # Use the api_endpoint, iterating through each page of the workouts with the maximum page size of 10.
         response = requests.get(api_endpoint + "workouts?page" + str(i) + "&pageSize=10",headers={"api-key":api_key})
         data = response.json()
-
-
 
 def main():
     return None
