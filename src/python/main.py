5fbecf4dcaa6e301c12021c5dd01f83d597864dc
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2025-01-27 12:43:38 -0700

    feat: :sparkles: populate_database function
    Add the workouts from the Hevy account into the database

--- src/python/main.py
+++ src/python/main.py
@@ -1,9 +1,7 @@
 import requests,json
 import sqlite3
+import os
 api_key = "1015d522-e3c1-49f7-9223-d275e947142a"
-
-api_endpoint = "https://api.hevyapp.com/v1/"
-
 
 def initialize_database():
     """
@@ -49,6 +47,76 @@ def get_all_initial_workouts(api_key,api_endpoint="https://api.hevyapp.com/v1/")
         current_page += 1
         
     return final_list
-
+def populate_database(start_clean=True):
+    """
+    Populate the database with the initial workouts.
+    (There may be unexpected behaviour if ran from any other instances.
+    If you need to update the database, please run the update_database function instead.)
+    :param start_clean: Whether to start with a clean database, default is True.
+    """
+    if start_clean:
+        try:
+            os.remove("database.db")
+        except OSError:
+            pass
+        initialize_database()
+    
+    conn = connect_database()
+     
+    workouts = get_all_initial_workouts(api_key)
+    
+    exercise_id = 1
+    set_id = 1
+    
+    for workout in workouts:
+        cursor = conn.cursor()
+        
+        workout_id = workout["id"]
+        title = workout["title"]
+        description = workout["description"]
+        start_time = workout["start_time"]
+        end_time = workout["end_time"]
+        updated_at = workout["updated_at"]
+        created_at = workout["created_at"]
+        
+        cursor.execute("INSERT INTO workouts "
+                       "VALUES (?,?,?,?,?,?,?)",
+                       (workout_id,title,description,start_time,end_time,updated_at,created_at))
+        cursor.commit()
+        
+        for exercise in workout["exercises"]:
+            exercise_index = exercise["index"]
+            exercise_title = exercise["exercise_title"]
+            exercise_notes = exercise["exercise_notes"]
+            
+            cursor.execute("INSERT INTO exercises "
+                           "VALUES (?,?,?,?,?)",
+                           (workout_id,exercise_id,exercise_index,exercise_title,exercise_notes))
+            
+            cursor.commit()
+            
+            for set in exercise["sets"]:
+                set_index = set["index"]
+                set_type = set["type"]
+                weight = set["weight_kg"]
+                reps = set["reps"]
+                distance = set["distance_meters"]
+                duration = set["duration_seconds"]
+                rpe = set["rpe"]
+                
+                cursor.execute("INSERT INTO sets "
+                               "VALUES (?,?,?,?,?,?,?,?,?)",
+                               (exercise_id,set_id,set_index,set_type,weight,reps,distance,duration,rpe))
+                
+                cursor.commit()
+                
+                set_id += 1
+                
+            exercise_id += 1         
+    
+    cursor.close()
+    conn.close()
+        
+    
 def main():
     return None
