60ba9b94ff9d5bfad1facf4ff3d2da8c8155d5fe
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2025-01-27 17:52:48 +0000

    feat: get all initial workouts dicts into list

--- src/python/main.py
+++ src/python/main.py
@@ -23,15 +23,31 @@ def connect_database():
     conn = sqlite3.connect("database.db")
 
     
-def get_all_initial_workouts():
-    i = 1
-    done = False
-    while not done:
+def get_all_initial_workouts(api_key,api_endpoint="https://api.hevyapp.com/v1/"):
+    """
+    Compile all the Hevy workouts from the API into a Python list (of dictionaries).
+    :return: A list of all the workouts.
+    """
+    current_page = 1
+    page_count = None
+    final_list = None
+    while current_page < page_count:
         # Use the api_endpoint, iterating through each page of the workouts with the maximum page size of 10.
         response = requests.get(api_endpoint + "workouts?page" + str(i) + "&pageSize=10",headers={"api-key":api_key})
-        data = response.json()
-
-
+        current_page = response.json()
+        if page_count is None:
+            page_count = current_page["page_count"]
+        
+        if final_list is None:
+            final_list = current_page["workouts"]
+        else: 
+            for workout in current_page["workouts"]:
+                final_list.append(workout)
+            
+        
+        current_page += 1
+        
+    return final_list
 
 def main():
     return None
