5a0bdbde1816154ffc05357e2ff12b26ec78150b
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2025-01-29 13:34:25 -0700

    chore: :art: Code cleanup

--- src/python/main.py
+++ src/python/main.py
@@ -38,7 +38,6 @@ def connect_database():
         raise Exception("Database does not exist. Please run the initialize_database function.")
     return sqlite3.connect("database.db")
 
-    
 def get_all_initial_workouts(api_key,api_endpoint="https://api.hevyapp.com/v1/"):
     """
     Compile all the Hevy workouts from the API into a Python list (of dictionaries).
@@ -66,7 +65,6 @@ def get_all_initial_workouts(api_key,api_endpoint="https://api.hevyapp.com/v1/")
     print("Finished compiling all workouts.")
         
     return final_list
-
 
 def populate_database(api_key,start_clean=True):
     """
@@ -139,6 +137,141 @@ def populate_database(api_key,start_clean=True):
     cursor.close()
     conn.close()
         
+def get_iso8601_date_from_string(date_string):
+    """
+    Convert a date string to an ISO8601 (ie. 1970-01-01T00:00:00Z) formatted date string with no offset.
+    It accepts dates formatted only in MM/DD/YYYY. (Sorry, everywhere except Canada and United States.)
+    :param date_string: The date string to convert.
+    :return: The ISO8601 formatted date string.
+    """
+    
+    components = date_string.split("/")
+    string = components[2]
+    string += "-"
+    
+    string += components[0]
+    string += "-"
+    
+    string += components[1]
+    string += "T00:00:00Z"
+    
+    return string
+
+def get_recent_workout_changes(date_since, api_endpoint="https://api.hevyapp.com/v1/"):
+    """
+    Use the Hevy API events endpoint to get a list of workout updates since a specific date.
+    This includes new, modified, and deleted workouts.
+    
+    Please see the README for how the API delivers the JSON response.
+    
+    :param date: The date to get the updates since, in ISO8601 format. (Example: "2021-01-01T00:00:00Z") (Note that hours and seconds matters; for the entire day, specify midnight. Offsets are not supported.)
+    """
+    updates = None
+    current_page = 1
+    page_count = None
+    
+    # Replace the colons in the date string with %3A to make it URL-safe.
+    new_str = []
+    for c in date_since:
+        if c == ":":
+            new_str += "%3A"
+        else:
+            new_str += c
+    
+    date_since = "".join(new_str)
+        
+    while current_page <= page_count or page_count is None:
+        response = requests.get(api_endpoint + "/workouts/events?page" + str(current_page) + "&pageSize=10&since=" + date_since,headers={"api-key":api_key})
+        current_page = response.json()
+        if page_count is None:
+            page_count = current_page["page_count"]
+        
+        if updates is None:
+            updates = current_page["events"]
+        else:
+            for event in current_page["events"]:
+                updates += event
+        current_page += 1
+    return updates
+
+def update_workout_locally(workout_id, data):
+    """
+    Update the workout (if it exists) with the given data.
+    :param: workout_id, the workout ID to update.
+    :param: data, the data to update the workout with.
+    """
+    
+    conn = connect_database()
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT * FROM workouts WHERE id = ?", (workout_id,))
+    result = cursor.fetchall()
+    
+    if result is None:
+        raise Exception("Workout not found.")
+    elif len(result) > 1:
+        raise Exception("More than one workout is found with this ID, somehow.")
+    else:
+        title = data["title"]
+        description = data["description"]
+        start_time = data["start_time"]
+        end_time = data["end_time"]
+        updated_at = data["updated_at"]
+        created_at = data["created_at"]
+        
+        cursor.execute("UPDATE workouts SET "
+                       "title=?,description=?,start_time=?,end_time=?,update_time=?,creation_time=? "
+                       "WHERE id =?",(title,description,start_time,end_time,updated_at,created_at,workout_id))
+        conn.commit()
+    
+    cursor.close()
+    conn.close()
+        
+def add_workout_locally(workout):
+    """
+    Add a workout to the database with the given data.
+    :param: workout, a dictionary with the workout data.
+    """
+    
+    id_to_search = workout["id"]
+    
+    conn = connect_database()
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT * FROM workouts WHERE id = ?",(id_to_search,))
+    result = cursor.fetchall()
+    if len(result) is not None:
+        raise Exception("There already exists a workout with this ID.")
+    else:
+        workout_id = workout["id"]
+        title = workout["title"]
+        description = workout["description"]
+        start_time = workout["start_time"]
+        end_time = workout["end_time"]
+        updated_at = workout["updated_at"]
+        created_at = workout["created_at"]
+        
+        cursor.execute("INSERT INTO workouts "
+                       "VALUES (?,?,?,?,?,?,?)", (workout_id,title,description,start_time,end_time,updated_at,created_at))
+        
+        conn.commit()
+    
+    cursor.close()
+    conn.close()
+    
+def delete_workout_locally(workout_id):
+    """
+    Delete a workout in the database.
+    :param: workout_id, in UUID format.
+    """
+    conn = connect_database()
+    cursor = conn.cursor()
+    
+    cursor.execute("DELETE FROM workouts WHERE id = ?",(workout_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+        
 def main_menu(api_key):
     """
     The main menu for the CLI.
@@ -183,136 +316,7 @@ def main_menu(api_key):
         
         if response == "6":
             sys.exit(0)
-def get_iso8601_date_from_string(date_string):
-    """
-    Convert a date string to an ISO8601 (ie. 1970-01-01T00:00:00Z) formatted date string with no offset.
-    It accepts dates formatted only in MM/DD/YYYY. (Sorry, everywhere except Canada and United States.)
-    :param date_string: The date string to convert.
-    :return: The ISO8601 formatted date string.
-    """
-    
-    components = date_string.split("/")
-    string = components[2]
-    string += "-"
-    
-    string += components[0]
-    string += "-"
-    
-    string += components[1]
-    string += "T00:00:00Z"
-    
-    return string
-def get_recent_workout_changes(date_since, api_endpoint="https://api.hevyapp.com/v1/"):
-    """
-    Use the Hevy API events endpoint to get a list of workout updates since a specific date.
-    This includes new, modified, and deleted workouts.
-    
-    Please see the README for how the API delivers the JSON response.
-    
-    :param date: The date to get the updates since, in ISO8601 format. (Example: "2021-01-01T00:00:00Z") (Note that hours and seconds matters; for the entire day, specify midnight. Offsets are not supported.)
-    """
-    updates = None
-    current_page = 1
-    page_count = None
-    
-    # Replace the colons in the date string with %3A to make it URL-safe.
-    new_str = []
-    for c in date_since:
-        if c == ":":
-            new_str += "%3A"
-        else:
-            new_str += c
-    
-    date_since = "".join(new_str)
-        
-    while current_page <= page_count or page_count is None:
-        response = requests.get(api_endpoint + "/workouts/events?page" + str(current_page) + "&pageSize=10&since=" + date_since,headers={"api-key":api_key})
-        current_page = response.json()
-        if page_count is None:
-            page_count = current_page["page_count"]
-        
-        if updates is None:
-            updates = current_page["events"]
-        else:
-            for event in current_page["events"]:
-                updates += event
-        current_page += 1
-    return updates
-def update_workout(workout_id, data):
-    """
-    Update the workout (if it exists) with the given data.
-    :param: workout_id, the workout ID to update.
-    :param: data, the data to update the workout with.
-    """
-    
-    conn = connect_database()
-    cursor = conn.cursor()
-    
-    cursor.execute("SELECT * FROM workouts WHERE id = ?", (workout_id,))
-    result = cursor.fetchall()
-    
-    if result is None:
-        raise Exception("Workout not found.")
-    elif len(result) > 1:
-        raise Exception("More than one workout is found with this ID, somehow.")
-    else:
-        title = data["title"]
-        description = data["description"]
-        start_time = data["start_time"]
-        end_time = data["end_time"]
-        updated_at = data["updated_at"]
-        created_at = data["created_at"]
-        
-        cursor.execute("UPDATE workouts SET "
-                       "title=?,description=?,start_time=?,end_time=?,update_time=?,creation_time=? "
-                       "WHERE id =?",(title,description,start_time,end_time,updated_at,created_at,workout_id))
-        conn.commit()
-    
-    cursor.close()
-    conn.close()
-def add_workout_locally(workout):
-    """
-    Add a workout to the database with the given data.
-    :param: workout, a dictionary with the workout data.
-    """
-    
-    id_to_search = workout["id"]
-    
-    conn = connect_database()
-    cursor = conn.cursor()
-    
-    cursor.execute("SELECT * FROM workouts WHERE id = ?",(id_to_search,))
-    result = cursor.fetchall()
-    if len(result) is not None:
-        raise Exception("There already exists a workout with this ID.")
-    else:
-        workout_id = workout["id"]
-        title = workout["title"]
-        description = workout["description"]
-        start_time = workout["start_time"]
-        end_time = workout["end_time"]
-        updated_at = workout["updated_at"]
-        created_at = workout["created_at"]
-        
-        cursor.execute("INSERT INTO workouts "
-                       "VALUES (?,?,?,?,?,?,?)", (workout_id,title,description,start_time,end_time,updated_at,created_at))
-        
-        conn.commit()
-    
-    cursor.close()
-    conn.close()
-def delete_workout_locally(workout_id):
-    """
-    Delete a workout in the database.
-    :param: workout_id, in UUID format.
-    """
-    conn = connect_database()
-    cursor = conn.cursor()
-    
-    cursor.execute("DELETE FROM workouts WHERE id = ?",(workout_id,))
-    conn.commit()
-    cursor.close()
-    conn.close()
+            
 def main():
     api_key = input("Please input the API key. (If you need help, please type 'help'): ")
     if api_key == "help":
